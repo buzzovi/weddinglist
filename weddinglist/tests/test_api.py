1455493a30904de741b7cd4d536b4e90c1b7c01b
Zoltan Buzas <buzzovi@gmail.com>
2020-09-22 18:09:00 +0100

    Test add

--- weddinglist/tests/test_api.py
+++ weddinglist/tests/test_api.py
@@ -22,7 +22,7 @@ class TestGiftList:
 
 class TestApi:
 
-    def test_api(self):
+    def test_gift_api(self):
         # GIVEN
         Gift.objects.create(name="Surprise", brand="Best")
         client = RequestsClient()
@@ -31,3 +31,59 @@ class TestApi:
         # THEN
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 1
+
+    def test_gift_list_api(self):
+        # GIVEN
+        GiftList.objects.create(name="Surprise")
+        client = RequestsClient()
+        # WHEN
+        response = client.get('http://127.0.0.1:8000/giftlists/?format=json')
+        # THEN
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 1
+
+    def test_purchasegift_api(self):
+        # GIVEN
+        gift_list = GiftList.objects.create(name="Surprise")
+        gift = Gift.objects.create(name="Surprise", brand="Best")
+        gift_list_item = GiftListItem.objects.create(gift=gift, gift_list=gift_list)
+        client = RequestsClient()
+        # WHEN
+        response = client.post('http://127.0.0.1:8000/purchasegift/'+str(gift_list_item.id))
+        # THEN
+        assert response.status_code == 200
+        assert json.loads(response.content) == "{'status':'OK'}"
+
+    def test_giftlistadd_api(self):
+        # GIVEN
+        gift_list = GiftList.objects.create(name="Surprise")
+        gift = Gift.objects.create(name="Surprise", brand="Best")
+        client = RequestsClient()
+        # WHEN
+        response = client.post(f'http://127.0.0.1:8000/giftlistadd/{gift_list.id}/{gift.id}')
+        # THEN
+        assert response.status_code == 200
+        assert json.loads(response.content) == "{'status':'OK'}"
+
+    def test_giftlistremove_api(self):
+        # GIVEN
+        gift_list = GiftList.objects.create(name="Surprise")
+        gift = Gift.objects.create(name="Surprise", brand="Best")
+        GiftListItem.objects.create(gift=gift, gift_list=gift_list)
+        client = RequestsClient()
+        # WHEN
+        response = client.post(f'http://127.0.0.1:8000/giftlistremove/{gift_list.id}/{gift.id}')
+        # THEN
+        assert response.status_code == 200
+        assert json.loads(response.content) == "{'status':'OK'}"
+
+    def test_report(self):
+        # GIVEN
+        gift_list = GiftList.objects.create(name="Surprise")
+        gift = Gift.objects.create(name="Surprise", brand="Best")
+        GiftListItem.objects.create(gift=gift, gift_list=gift_list)
+        client = RequestsClient()
+        # WHEN
+        response = client.get('http://127.0.0.1:8000/report/1')
+        # THEN
+        assert response.status_code == 200
