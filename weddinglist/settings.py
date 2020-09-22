7bf5a35ce87ac6b0b90ea812bcf5608b051a63b7
Zoltan Buzas <buzzovi@gmail.com>
2020-09-22 18:12:42 +0100

    Settings

--- weddinglist/settings.py
+++ weddinglist/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wai3mr25+!t2=7+x^9&&jdyx)-@5h^rujg723_sgmymg0(7$xz'
+SECRET_KEY = config(
+    'SECRET_KEY', default='wai3mr25+!t2=7+x^9&&jdyx)-@5h^rujg723_sgmymg0(7$xz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
